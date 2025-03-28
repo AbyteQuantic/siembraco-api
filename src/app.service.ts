b4986c33db28d365e9c7ebdd63737cb409c74a0d
abytecorp <abytecorp@gmail.com>
2025-03-28 17:49:13 -0500

    change message

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome to SiembraCO API, A_byte service auxpicied';
+    return 'Welcome to SiembraCO API,  this is an A_byte service auxpicied';
   }
 }
